c83b52680634a8290641201ab0aaf730f190f004
ahmedhegazy20 <124202885+ahmedhegazy20@users.noreply.github.com>
2023-05-14 00:33:44 +0200

    main.c

--- main.c
+++ main.c
@@ -22,54 +22,54 @@ int main(){
 	initials_PORTF();
 	//init_LCD();
 	//LCD_outstring ("hello ");
- 
-	
+
+
 	while (1){
-	
+
  GPS_read (current_position) ;
-	
+
   if (previous_position [0]==0 | previous_position [1]==0){
- previous_position [0]= current_position [0]; 
- previous_position [1]= current_position [1] ; 
- 
+ previous_position [0]= current_position [0];
+ previous_position [1]= current_position [1] ;
+
  }
  dist += Distance_calculate( previous_position [1] , previous_position [0], current_position [1] ,current_position [0]);
  previous_position [0]= current_position [0] ;
 previous_position [1]= current_position [1] ;
- 
- 
-	remaining_displacement	=Distance_calculate(previous_position [0] , previous_position [1] ,30.253684, 31.466645 );  
- 
- if (remaining_displacement<=5){    
-   //LCD_outstring ("Target Reached"); 	 
+
+
+	remaining_displacement	=Distance_calculate(previous_position [0] , previous_position [1] ,30.253684, 31.466645 );
+
+ if (remaining_displacement<=5){
+   //LCD_outstring ("Target Reached");
 	 GPIO_PORTF_DATA_R |= 0X08; //greenledon
 	 systick_wait(700); //delay
  }
- 
+
  //else if  (remaining_displacement < 5){
 	// GPIO_PORTF_DATA_R |= 0X0C ; // YELLOW ON)
- //delay 1sec 
-	 
-	
+ //delay 1sec
+
+
 
 //float_to_char_array(dist, number, sizeof(number));
 //void LCD_init(void);
 //	LCD_outstring ("Distance: ");
 //LCD_outstring (number);
-//systick_wait(400); 
+//systick_wait(400);
 
  //}
-	 
-	 
+
+
  else {
 	 GPIO_PORTF_DATA_R |= 0X02 ;// RED ON
- //delay 1 sec 
+ //delay 1 sec
  systick_wait(700);
-	 
+
 	//float_to_char_array(dist, number, sizeof(number));
 //void LCD_init(void);
 	// LCD_outstring ("Distance: /n");
-//LCD_outstring (number);
-	 
+//LCD_outstring  (number);
+
  }
  } }
